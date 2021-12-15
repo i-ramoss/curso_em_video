af95b90c6c4e0375e500464aaa0d506e23030129
Ian Ramos <ianramossantos@hotmail.com>
2021-12-15 13:06:56 -0300

    class code 6

---
+++ python3_module01_fundamentals/classes/class_06.py
@@ -0,0 +1,14 @@
+number01 = int(input('Enter a number: '))
+number02 = int(input('Enter a number: '))
+
+value = input('Enter a value: ')
+
+print('The sum between {} and {} is {}'.format(number01, number02, number01 + number02))
+
+print(type (number01))
+
+print(value.isnumeric())
+print(value.isalpha())
+print(value.isupper())
+print(value.islower())
+print(value.isprintable())
