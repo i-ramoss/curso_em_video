e537bb9cc0dca14b8d8b8894b7eb15c4c0745740
Ian Ramos <ianramossantos@hotmail.com>
2021-12-15 11:32:21 -0300

    code of challenges 1-3

---
+++ python3_module01_fundamentals/challenges/01_welcome.py
@@ -0,0 +1,6 @@
+# Create a Python script that reads a persons's name and displays a welcome message according to the value entered
+
+name = input('What is your name? ')
+
+print('Welcome,', name + '. Nice to meet you!')
+print('Welcome, {}'.format(name) + '. Nice to meet you!')
---
+++ python3_module01_fundamentals/challenges/03_sum.py
@@ -0,0 +1,6 @@
+# Create a Python script that reads two numbers and display s the sum between them.
+
+number01 = int(input('Enter the first number: '))
+number02 = int(input('Enter the second number: '))
+
+print('The sum between them is:', number01 + number02)
