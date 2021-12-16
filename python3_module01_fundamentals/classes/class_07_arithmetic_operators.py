9bf9f2f1f55ae8c12cf3e0c29fc8406ff25b2bd7
Ian Ramos <ianramossantos@hotmail.com>
2021-12-16 10:35:20 -0300

    class code 07 about arithmetic operators

---
+++ python3_module01_fundamentals/classes/class_07_arithmetic_operators.py
@@ -0,0 +1,8 @@
+# name = input('What is your name? ')
+# print('Nice to meet you {:=^20}!'.format(name))
+
+number01 = 4
+number02 = 3
+
+print('The sum between {} and {} is {}, \nthe product is {} and the division is {:.2f}.'.format(number01, number02, number01 + number02, number01 * number02, number01 / number02), end=' ')
+print('The integer division is {} and the power is {}'.format(number01 // number02, number01 ** number02))
