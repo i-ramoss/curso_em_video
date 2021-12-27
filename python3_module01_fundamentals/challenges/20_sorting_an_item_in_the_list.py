5e9765e87f9dae1f111311fa1b28b5d009c633a1
Ian Ramos <ianramossantos@hotmail.com>
2021-12-27 16:53:07 -0300

    code of challenges 20-21 about modules

---
+++ python3_module01_fundamentals/challenges/20_sorting_an_item_in_the_list.py
@@ -0,0 +1,13 @@
+# The same teacher from the previous challenge wants to draw the order in which student's works is presented. Make a program that reads the names of the four students and shows the order drawn.
+
+from random import sample
+
+student01 = input('Enter a student name: ')
+student02 = input('Enter a second student name: ')
+student03 = input('Enter a third student name: ')
+student04 = input('Enter a fourth student name: ')
+
+
+students = [student01, student02, student03, student04]
+
+print('The order of presentation of students will be: {}'.format(sample(students, k=len(students))))
---
+++ python3_module01_fundamentals/challenges/21_playing_a_mp3.py
@@ -0,0 +1,5 @@
+# Make a python program that opens and plays the audio of an mp3 file
+
+import playsound
+
+playsound.playsound('bala_azul.mp3')
