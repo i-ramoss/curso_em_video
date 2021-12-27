9c4a7343be3d94c0ff5e40ede50a717ce56306b0
Ian Ramos <ianramossantos@hotmail.com>
2021-12-27 17:29:41 -0300

    class code 09 about strings

---
+++ python3_module01_fundamentals/classes/class_09_manipulating_text.py
@@ -0,0 +1,41 @@
+
+text = 'Course In Video'
+
+# Slicing
+
+print(text[10:15:2])
+
+# Analysis
+
+print(len(text))
+print(text.count('o'))
+print(text.find('deo'))
+print(text.find('life'))
+print('Course' in text)
+
+# Transformation
+
+print(text.replace('Video', 'MacOS'))
+print(text.upper())
+print(text.lower())
+print(text.capitalize())
+print(text.title())
+
+text = '   Learn Python   '
+
+print(text.strip())
+print(text.rstrip())
+print(text.lstrip())
+
+# Division
+
+print(text.split())
+print(text.split()[1][0])
+
+# Join
+
+print('-'.join(text))
+
+
+print('''Welcome!
+Are you completely new to programming?''')
