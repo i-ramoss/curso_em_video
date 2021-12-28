ec628d5c58c3a90b281c482775f5de4e35a3012c
Ian Ramos <ianramossantos@hotmail.com>
2021-12-28 17:31:32 -0300

    class code 11 about colors on terminal

---
+++ python3_module01_fundamentals/classes/class_11_colors_on_the_terminal.py
@@ -0,0 +1,8 @@
+name = 'Flora Ramos'
+
+colors = {'clean': '\033[m', 
+          'blue': '\033[34m', 
+          'yellow': '\033[33m', 
+          'blackAndWhite': '\033[7;30m'}
+
+print('Hi {}{}{}! Nice to meet you!'.format(colors['yellow'], name, colors['clean']))
