27ed83e1883a5c5ec86880f8a890d88f4725d4fa
Ian Ramos <ianramossantos@hotmail.com>
2022-03-16 16:35:52 -0300

    feat: create a program to show the multiplication table of a number using for loop

---
+++ python3_module02_control_structures/challenges/49_multiplication_table_v2.0_.py
@@ -0,0 +1,9 @@
+# Redo a challenge09, showing the multiplication table of a number that the user 
+# choose, only now using for loop
+
+# Make a program that reads any integer and displayss your table
+
+number = int(input('Enter a number to display its multiplication table: '))
+
+for index in range(0, 11):
+    print('{} X {:2} == {}'.format(number, index, number * index))
