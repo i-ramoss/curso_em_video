926ac490aa460e7d20f7158797fb515e03a7ccd1
Ian Ramos <ianramossantos@hotmail.com>
2022-03-16 16:26:45 -0300

    feat: create a script to displays al even numbers between 1 and 50

---
+++ python3_module02_control_structures/challenges/47_pair_count.py
@@ -0,0 +1,7 @@
+# Create a program that displays on the screen all even numbers that are in the
+# range between 1 and 50.
+
+for current in range(2, 51, 2):
+    print(current, end=" ")
+        
+print("Finish")
