b2ddbb8176ebbc82bd943a72ea24564d21902a86
Ian Ramos <ianramossantos@hotmail.com>
2022-08-19 21:30:26 -0300

    chore: create more attributes for pen class

--- poo_with_java/classes/class_04_getter_setter_and_constructor_methods/Pen.java
+++ poo_with_java/classes/class_04_getter_setter_and_constructor_methods/Pen.java
@@ -2,7 +2,9 @@ package classes.class_04_getter_setter_and_constructor_methods;
 
 public class Pen {
   private String model;
+  private String color;
   private float nib;
+  private boolean isCovered;
 
   public String getModel() {
     return this.model;
@@ -10,6 +12,14 @@ public class Pen {
 
   public void setModel(String model) {
     this.model = model;
+  }
+
+  public String getColor() {
+    return this.color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
   }
 
   public float getNib() {
@@ -20,8 +30,22 @@ public class Pen {
     this.nib = nib;
   }
 
+  public boolean getIsCovered() {
+    return this.isCovered;
+  }
+
+  public void setIsCovered() {
+    this.isCovered = true;
+  }
+
+  public void setIsNotCovered() {
+    this.isCovered = false;
+  }
+
   public void status() {
     System.out.println("Model: " + this.getModel());
     System.out.println("Nib: " + this.getNib());
+    System.out.println("Color: " + this.getColor());
+    System.out.println("Is covered : " + this.getIsCovered());
   }
 }
